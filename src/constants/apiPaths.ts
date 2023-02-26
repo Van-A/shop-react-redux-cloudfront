ee91d0e5c560cfa99c815d9607a0887ff05456d8
Andrei Vaniushyn <vanushin.andrey@gmail.com>
2023-02-26 12:36:10 +0300

    task-3 (First API with AWS API Gateway and AWS Lambda)

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,4 +1,5 @@
 const API_PATHS = {
+  default: "https://206h5btyic.execute-api.us-east-1.amazonaws.com",
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
