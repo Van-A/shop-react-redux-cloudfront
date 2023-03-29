05904f5261151e6aced0fcad1041f5da5df9e8a7
Andrei Vaniushyn <vanushin.andrey@gmail.com>
2023-03-29 18:09:38 +0300

    task-5 Integration with S3

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   default: "https://206h5btyic.execute-api.us-east-1.amazonaws.com",
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://1do5yxpid6.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
